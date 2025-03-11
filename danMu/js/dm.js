9d9914ea47767a4fea897729cb650378fd0d39c5
mfandcc <45775793+mfandcc@users.noreply.github.com>
2025-03-11 11:22:05 +0800

    Create dm.js

---
+++ danMu/js/dm.js
@@ -0,0 +1,15 @@
+import {} from '../../core/uz3lib.js'
+import {} from '../../core/uzUtils.js'
+
+const appConfig = {
+    _uzTag: '',
+    get uzTag() {
+        return this._uzTag
+    },
+    set uzTag(value) {
+        this._uzTag = value
+    },
+}
+async function searchDanMu(item) {
+    return null
+}
